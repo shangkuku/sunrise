256255d37905467c407c8df0ae62513a26f57854
shangkuku <007_gudan@163.com>
2018-01-19 18:12:47 +0800

    Initial commit

---
+++ src/main/java/rbk/sunrise/SunriseApplication.java
@@ -0,0 +1,12 @@
+package rbk.sunrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunriseApplication.class, args);
+	}
+}
