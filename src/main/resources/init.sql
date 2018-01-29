141420c5d4023916341a6f5f2a99b828ad98ac01
shangkuku <007_gudan@163.com>
2018-01-30 00:42:55 +0800

    V2

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,77 +1,40 @@
-/*
-SQLyog Ultimate v11.24 (64 bit)
-MySQL - 5.6.39-log : Database - sunrise
-*********************************************************************
-*/
+create table rbk_role
+(
+	id bigint not null auto_increment
+		primary key,
+	name varchar(32) null,
+	code varchar(32) null
+)
+;
 
-/*!40101 SET NAMES utf8 */;
+create table rbk_role_permission
+(
+	id bigint not null auto_increment
+		primary key,
+	role_id bigint null,
+	permission int null,
+	module varchar(32) null
+)
+;
 
-/*!40101 SET SQL_MODE=''*/;
+create table rbk_user
+(
+	id bigint not null
+		primary key,
+	name varchar(32) null,
+	password varchar(32) null,
+	create_time datetime null,
+	salt varchar(128) null,
+	mobile_phone varchar(32) null
+)
+;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`sunrise` /*!40100 DEFAULT CHARACTER SET utf8 */;
+create table rbk_user_role
+(
+	id bigint not null auto_increment
+		primary key,
+	role_id bigint null,
+	user_id bigint null
+)
+;
 
-USE `sunrise`;
-
-/*Table structure for table `role` */
-
-DROP TABLE IF EXISTS `role`;
-
-CREATE TABLE `role` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) DEFAULT NULL,
-  `code` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `role` */
-
-/*Table structure for table `role_permission` */
-
-DROP TABLE IF EXISTS `role_permission`;
-
-CREATE TABLE `role_permission` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `role_id` bigint(20) DEFAULT NULL,
-  `permission` int(11) DEFAULT NULL,
-  `module` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `role_permission` */
-
-/*Table structure for table `user` */
-
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user` (
-  `id` bigint(20) DEFAULT NULL,
-  `name` varchar(32) DEFAULT NULL,
-  `password` varchar(32) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
-  `salt` varchar(128) DEFAULT NULL,
-  `mobile_phone` varchar(32) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `user` */
-
-/*Table structure for table `user_role` */
-
-DROP TABLE IF EXISTS `user_role`;
-
-CREATE TABLE `user_role` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `role_id` bigint(20) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `user_role` */
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
