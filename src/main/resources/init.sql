ee1d2408c6527f4fdb2f4116be20793ce458ca01
shangkuku <007_gudan@163.com>
2018-01-31 01:48:51 +0800

    V2

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -12,18 +12,18 @@ create table rbk_role_permission
 	id bigint not null auto_increment
 		primary key,
 	role_id bigint null,
-	permission VARCHAR(64) null
+	permission int null,
+	module varchar(32) null
 )
 ;
 
 create table rbk_user
 (
-	id bigint not null
+	id bigint not null auto_increment
 		primary key,
 	name varchar(32) null,
-	password varchar(32) null,
+	password varchar(256) null,
 	create_time datetime null,
-	salt varchar(128) null,
 	mobile_phone varchar(32) null
 )
 ;
